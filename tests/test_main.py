ae61d84c0708e1d57381491573af5df8a3ff3d96
haw230 <hancheng.wu22@gmail.com>
2017-07-18 15:27:32 -0600

    Add files via upload

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,18 @@
+import unittest
 
+import sys
+sys.path.insert(0, '/cshome/ha10/Documents/linear_search/search/')
+
+from cshome import *
+
+class TestLinearSearch(unittest.TestCase):
+    def testing(self):
+        self.assertEqual(search([0,1,2], 1), 1)
+        self.assertEqual(search([1,2,3], 2), 1)
+        self.assertEqual(search([1,2,3], 3), 3)
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
