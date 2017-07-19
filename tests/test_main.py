54a0685c25d6a3e3dccceb8a87b647d0f0b37ae8
haw230 <hancheng.wu22@gmail.com>
2017-07-19 13:38:10 -0600

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,12 @@
 import unittest
 import sys
-sys.path.insert(0, '/home/ubuntu/workspace/linear_search')
+sys.path.insert(0, '/home/ubuntu/workspace/linear_search') #for main.py
+sys.path.insert(0, '/home/ubuntu/workspace/solution') #solved_search.py
 from main import search
 
 class TestLinearSearch(unittest.TestCase):
     def testing(self):
-        self.assertEqual(search([0,1,2], 1), 1)
+        self.assertEqual(search([0,1,2], 1), solved_search([0,1,2], 1))
         self.assertEqual(search([1,2,3], 2), 1)
         self.assertEqual(search([1,2,3], 3), 3)
 
