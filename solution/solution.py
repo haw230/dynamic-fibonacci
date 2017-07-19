ff2f9dd61d729d6b572f5b8410dc7086be1f44de
haw230 <hancheng.wu22@gmail.com>
2017-07-19 13:38:43 -0600

    Update and rename search.py to solution.py

--- solution/solution.py
+++ solution/solution.py
@@ -1,4 +1,4 @@
-def search(ls, num):
+def solved_search(ls, num):
     for i in range(len(ls)): #for every index of ls, starting from 0, going up by one
         if ls[i] == num: #if the value of this index is what we're looking for
             return i #that is the location, function is complete
