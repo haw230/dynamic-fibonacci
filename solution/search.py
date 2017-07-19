8de2fe1063a001297dc7d3bf6bcf5e33767689c7
haw230 <hancheng.wu22@gmail.com>
2017-07-19 13:35:52 -0600

    Update search.py

--- solution/search.py
+++ solution/search.py
@@ -1,1 +1,6 @@
 def search(ls, num):
+    for i in range(len(ls)): #for every index of ls, starting from 0, going up by one
+        if ls[i] == num: #if the value of this index is what we're looking for
+            return i #that is the location, function is complete
+
+    return -1 #by the time the code here is reached, it means "return i" has never been reached
