2cfa9be4a3305c3d33fe41ee03510481759cf6c7
haw230 <hancheng.wu22@gmail.com>
2017-07-20 14:02:15 -0600

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,21 +1,26 @@
-import unittest
 import sys
 
 sys.path.insert(0, '/home/ubuntu/workspace/linear_search') #for main.py
 sys.path.insert(0, '/home/ubuntu/workspace/solution') #solved_search.py
 
-from main import search
-from solution import solved_search
+from main import func
+from solution import solved_algo
+    
+def simple_test():
+    assert(func()) == solved_algo()
+    assert(func()) == solved_algo()
+    assert(func()) == solved_algo()
+    
+def rand_test():
+    pass
 
-
-class TestLinearSearch(unittest.TestCase):
-    def testing(self):
-        self.assertEqual(search([0,1,2], 1), solved_search([0,1,2], 1))
-        #self.assertEqual(search([1,2,3], 2), 1)
-        #self.assertEqual(search([1,2,3], 3), 3)
-        
+def hard_test():
+    pass
+            
 def main():
-    unittest.main()
+    simple_test()
+    rand_test()
+    hard_test()
 
 if __name__ == '__main__':
     main()
