acd2f21087eeaf35399dbd528057753d80629b4d
hanchengwu <hancheng.wu22@gmail.com>
2017-07-25 19:01:31 +0000

    Polished the code

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
 import sys
 
-sys.path.insert(0, '/home/ubuntu/workspace/algorithm_name') #main.py
-sys.path.insert(0, '/home/ubuntu/workspace/solution') #solution.py
+sys.path.insert(0, '/home/ubuntu/workspace/algorithm_name') #change this to correct folder name
+sys.path.insert(0, '/home/ubuntu/workspace/solution')
 
-from main import algorithm_name
-from solution import solved_algorithm_name
+from main import algorithm_name #change to proper function name
+from solution import solved_algorithm_name #change to proper function name
 from time import sleep
-
-
-
-class aethetics(object): #https://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
+class aethetics(object):
     BLUE = '\033[94m'
     GREEN = '\033[92m'
-    WARNING = '\033[93m' #yellow
-    FAIL = '\033[91m' #red
-    END = '\033[0m' #must put at the end of every LINE
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    END = '\033[0m'
     BOLD = '\033[1m'
-    LINE = '———————————————————————————————————————————————————————————————'
+    LINE = '\033[95m———————————————————————————————————————————————————————————————\033[0m'
 
 class test_cases(object):
     def __init__(self):
@@ -25,23 +22,24 @@ class test_cases(object):
         self.total_tests = 0
 
     def tests(self): #add tests here
-        self.test(algorithm_name, solved_algorithm_name, [1, 3, 2])
-        self.test(algorithm_name, solved_algorithm_name, [1, 2, 3])
-        self.test(algorithm_name, solved_algorithm_name, [3, 2, 1])
+        f1,f2 = algorithm_name, solved_algorithm_name #change to proper function names
+        self.test(f1, f2, [1, 3, 2])
+        self.test(f1, f2, [1, 2, 3])
+        self.test(f1, f2, [3, 2, 1])
     
     def test(self, func1, func2, ls):
         ls1 = list(ls)
         x, y = func1(ls), func2(ls1)
         if(x == y):
             print(aethetics.GREEN + 'Test passed with param of ' + aethetics.BLUE + str(ls) + aethetics.END + '\n' + aethetics.END)
-            print(aethetics.BOLD + str(x) + aethetics.END + ' matches the answer ' + aethetics.BOLD + str(Y) + aethetics.END)
+            print(aethetics.BOLD + str(x) + aethetics.END + ' matches the answer ' + aethetics.BOLD + str(y) + aethetics.END)
             self.passed_tests += 1
         else:
             print(aethetics.FAIL + 'Test failed with param of ' + aethetics.BLUE + str(ls) + aethetics.END + '\n' + aethetics.END)
             print(aethetics.BOLD + str(x) + aethetics.END + ' does not match the answer ' + aethetics.BOLD + str(y) + aethetics.END)
         self.total_tests += 1
         print(aethetics.LINE + '\n')
-        sleep(0.6)
+        sleep(0.7)
         
     def end(self):
         if(self.passed_tests == self.total_tests):
@@ -49,8 +47,9 @@ class test_cases(object):
         else:
             print(aethetics.WARNING + 'Passed ' +  str(self.passed_tests) + ' of ' + str(self.total_tests) + ' tests.' + aethetics.END)
 
-if __name__ == '__main__':
-    print('\n' + aethetics.GREEN + "Running Tests..."  + '\n' + aethetics.END)
+if(__name__ == '__main__'):
+    print('\n' + aethetics.GREEN + "Running Tests..." + aethetics.END)
+    print(aethetics.LINE + '\n')
     t = test_cases()
     t.tests()
     t.end()
