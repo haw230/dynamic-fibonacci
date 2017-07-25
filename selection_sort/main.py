f2f53f95d378684500c62c773c91809ffb04c13f
hanchengwu <hancheng.wu22@gmail.com>
2017-07-25 17:11:37 +0000

    est

--- selection_sort/main.py
+++ selection_sort/main.py
@@ -1,4 +1,2 @@
-#!/home/ubuntu/workspace/selection_sort python
-def selection_sort(ls): #Write function name and parameters necessary. "func" will be used again in test_main.py.
-    return ls
-  #pass
+def selection_sort(ls):
+    pass
--- tests/test_main.py
+++ tests/test_main.py
@@ -11,46 +11,43 @@ from time import sleep
 
 
 class aethetics(object): #https://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
-    PINK = '\033[95m'
     BLUE = '\033[94m'
     GREEN = '\033[92m'
     WARNING = '\033[93m' #yellow
     FAIL = '\033[91m' #red
     END = '\033[0m' #must put at the end of every LINE
     BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
     LINE = '———————————————————————————————————————————————————————————————'
 
 class test_cases(object):
     def __init__(self):
         self.passed_tests = 0
         self.total_tests = 0
+
+    def tests(self): #add tests here
+        self.test(selection_sort, solved_selection_sort, [1, 3, 2])
+        self.test(selection_sort, solved_selection_sort, [1, 2, 3])
+        self.test(selection_sort, solved_selection_sort, [3, 2, 1])
     
     def test(self, func1, func2, ls):
         ls1 = list(ls)
         x, y = func1(ls), func2(ls1)
         if(x == y):
-            print(aethetics.GREEN + 'Test passed with param of ' + str(ls) + '\n' + aethetics.END)
-            print(str(x) + ' matches the answer ' + str(y))
+            print(aethetics.GREEN + 'Test passed with param of ' + aethetics.BLUE + str(ls) + aethetics.END + '\n' + aethetics.END)
+            print(aethetics.BOLD + str(x) + aethetics.END + ' matches the answer ' + aethetics.BOLD + str(Y) + aethetics.END)
             self.passed_tests += 1
         else:
-            print(aethetics.FAIL + 'Test failed with param of ' + str(ls) + '\n' + aethetics.END)
-            print(str(x) + ' does not match the answer ' + str(y))
+            print(aethetics.FAIL + 'Test failed with param of ' + aethetics.BLUE + str(ls) + aethetics.END + '\n' + aethetics.END)
+            print(aethetics.BOLD + str(x) + aethetics.END + ' does not match the answer ' + aethetics.BOLD + str(y) + aethetics.END)
         self.total_tests += 1
         print(aethetics.LINE + '\n')
-        sleep(0.7)
+        sleep(0.6)
         
     def end(self):
         if(self.passed_tests == self.total_tests):
             print(aethetics.GREEN + 'All ' + str(self.total_tests) + ' tests passed.' + aethetics.END)
         else:
             print(aethetics.WARNING + 'Passed ' +  str(self.passed_tests) + ' of ' + str(self.total_tests) + ' tests.' + aethetics.END)
-    
-    def tests(self):
-        self.test(selection_sort, solved_selection_sort, [1, 3, 2])
-        self.test(selection_sort, solved_selection_sort, [1, 2, 3])
-        self.test(selection_sort, solved_selection_sort, [3, 2, 1])
-    
 
 if __name__ == '__main__':
     print('\n' + aethetics.GREEN + "Running Tests..."  + '\n' + aethetics.END)
